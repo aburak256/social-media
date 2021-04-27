1fe9413ce46d9609b1818a7a35d3a0f814c2dd77
aburak256 <aburak256@gmail.com>
2021-04-28 01:36:16 +0300

    Add Chakra UI to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ChakraProvider } from "@chakra-ui/react";
 
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-ReactDOM.render( < App / > ,
-    document.getElementById('root')
-);
+ReactDOM.render(
+    <React.StrictMode>
+      <ChakraProvider resetCSS>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
