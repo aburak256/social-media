eefcf5156d1b4ed01755303de03fa8664a3190ac
aburak256 <aburak256@gmail.com>
2021-04-30 22:04:08 +0300

    Changed get links to created API

--- src/components/TopicList.js
+++ src/components/TopicList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { VStack, Box, Badge, HStack, Text} from '@chakra-ui/layout';
 import { ViewIcon } from '@chakra-ui/icons'
+import { Image } from "@chakra-ui/react"
+import {API} from "aws-amplify";
 
 
 export default class PostList extends React.Component {
@@ -9,24 +11,23 @@ export default class PostList extends React.Component {
     topics: []
   }
 
-  componentDidMount() {
-    axios.get(`https://my-json-server.typicode.com/typicode/demo/posts`)
-      .then(res => {
-        const topics = res.data;
-        console.log(topics)
-        this.setState({ topics });
-      })
-  }
+  async componentDidMount() {
+    const data = await API.get(`topicsApi`, '/topics')
+    const topics = data
+    console.log(topics)
+    this.setState({ topics })
+    }
 
   render() {
     return (
         <VStack w="80%">     
           { this.state.topics.map(topic => 
-          <Box w="80%" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="lg">         
+          <Box w="80%" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="lg">  
+            <Image src={topic.imageURL} alt={topic.topic} />       
             <Box p="6">
               <Box alignItems="baseline">
                 <Badge borderRadius="full" px="2" colorScheme="teal">
-                {topic.title} - This will be topic title 
+                {topic.topic}
                 </Badge>
               </Box>     
               <Text
@@ -35,14 +36,14 @@ export default class PostList extends React.Component {
                 lineHeight="tight"
                 noOfLines={[1, 2, 3]}
               >
-                This is the topic description
+                { topic.text }
               </Text>
       
               <Box d="flex" mt="2" alignItems="center">
                 {/* This will be some kind of a popularity metric*/}                
                 <ViewIcon  color="teal.500"/>
                 <Box as="span" ml="2" color="gray.600" fontSize="md">
-                  4845
+                  {topic.numberOfFollowers}
                 </Box>
               </Box>             
             </Box>
