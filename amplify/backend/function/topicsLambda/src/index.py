24244e0b0356711723bebdd7cbf80dd1a8fa6b12
aburak256 <aburak256@gmail.com>
2021-05-02 03:31:00 +0300

    Added: Amplify get posts in topic

--- amplify/backend/function/topicsLambda/src/index.py
+++ amplify/backend/function/topicsLambda/src/index.py
@@ -7,35 +7,64 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('SingleTableDesign')
 
 def handler(event, context):
-  print('received event:')
-  print(event)
+    print('received event:')
+    print(event)
+    path = event['resource']
 
-  if event['resource'] == "/topics":
-    resp = table.scan(
-        # Scanning because in this GSI there are only few elements
-        IndexName="topic-index",
-    )
+    if event['resource'] == "/topics":
+        resp = table.scan(
+            # Scanning because in this GSI there are only few elements
+            IndexName="topic-index",
+        )
 
-    print(resp['Items'])
-    response = {
-        'statusCode': 200,
-        'body': json.dumps(resp['Items']),
+        print(resp['Items'])
+        response = {
+            'statusCode': 200,
+            'body': json.dumps(resp['Items']),
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+        }
+
+        return response
+
+    if "/topics" in event['resource']:
+        params = event['pathParameters']
+        topic = params['proxy']
+        resp = table.query(
+            KeyConditionExpression=Key('PK').eq("TOPIC#" + topic + "#POST")
+        )
+
+        print(resp['Items'])
+        posts = []
+        for item in resp['Items']:
+            response = table.query(
+                KeyConditionExpression=Key('PK').eq("POST#" + item['postId'])
+        )
+            posts.append(response['Items'][0])
+        
+        print(posts)
+        response = {
+            'statusCode': 200,
+            'body': json.dumps(posts),
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+        }
+
+        return response
+
+  
+    return {
+        'statusCode': 404,
         'headers': {
             'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
+        'body': json.dumps("Couldn't find the request")
     }
-
-    return response
-
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
