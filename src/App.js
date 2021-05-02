59f946aa07929d11ee1096b5f0a4850bc3bd6708
aburak256 <aburak256@gmail.com>
2021-05-02 03:32:09 +0300

    Add: Route to topic detail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Signup from './components/pages/Signup';
 import Topics from './components/pages/Topics';
+import TopicDetail from './components/pages/TopicDetail';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Amplify, {API} from "aws-amplify";
 import awsExports from "./aws-exports";
@@ -20,6 +21,7 @@ function App() {
                     <Route path='/' exact component={Home} />
                     <Route path='/sign-up' exact component={Signup} />
                     <Route path='/topics' exact component={Topics} />
+                    <Route exact path="/topics/:topic" component={TopicDetail} />
                     {/* <Route path='/about' exact component={About} /> */}
                 </Switch>
             </Router>
