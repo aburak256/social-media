dde6df570f6cdffee38372a444aec975e1ab6c30
aburak256 <aburak256@gmail.com>
2021-05-02 03:33:27 +0300

    Add: Topic details page

---
+++ src/components/pages/TopicDetail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../App.css';
+import PostList from '../PostList';
+import {Center, Box, VStack} from "@chakra-ui/react"
+import { useParams } from 'react-router';
+
+function TopicDetail() {
+    const {topic} = useParams()
+    return (        
+        <div>
+            <title>Topics</title>
+            <Center> 
+                <VStack spacing="24px" width="70%">            
+                    <h2>This is the posts page</h2>
+                    <br/>
+                    <PostList topic={topic}/>
+                </VStack>
+            </Center>
+        </div>
+    )
+}
+
+export default TopicDetail
