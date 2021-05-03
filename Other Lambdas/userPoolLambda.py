c6be14bcdf4b596b35ddfe3f777253616ec8938c
aburak256 <aburak256@gmail.com>
2021-05-03 16:49:36 +0300

    Add user info to dynamoDB when user created from cognito

---
+++ Other Lambdas/userPoolLambda.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+
+from boto3.dynamodb.conditions import Key
+
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+table = dynamodb.Table('SingleTableDesign')
+
+def lambda_handler(event, context):
+    print("Recieved Event in user handler:", event)
+    
+    if event['triggerSource'] == "PostConfirmation_ConfirmSignUp" and event['userPoolId'] == 'us-east-2_TmvLiBZyZ':
+        username = event['userName']
+        attr = event['request']['userAttributes']
+        if 'phone_number' in attr:
+            phone = attr['phone_number']
+        else:
+            phone = ''
+        response = table.put_item(
+           Item={
+                'PK': "USER#" + attr['sub'],
+                'sortKey': "METADATA",
+                'text' : 'Bio',
+                'numberOfFollowers': '0',
+                'numberOfFollows': '0',
+                'mail' : attr['email'],
+                'userName': username,
+                'phone': phone,
+            }
+        )
+        return event
+    else:
+        print("Failed request")
