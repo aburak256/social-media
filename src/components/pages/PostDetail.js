657b765b5991b28011f98279996946d8f6f13572
aburak256 <aburak256@gmail.com>
2021-05-05 04:29:13 +0300

    Create post detail page

---
+++ src/components/pages/PostDetail.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import '../../App.css';
+import {Center, Box, VStack} from "@chakra-ui/react"
+import Post from '../Post';
+
+function PostDetail() {
+    const {post} = useParams()
+    return (
+        <div>
+            <title>Post</title>
+            <Center> 
+                <VStack spacing="24px" width="70%">            
+                    <h2>This is the post detail page</h2>
+                    <br/>
+                    <Post post={post}/>
+                </VStack>
+            </Center>
+        </div>
+    )
+}
+
+export default PostDetail
