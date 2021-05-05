c12192395587ad85e944900cbb6f0ca6e0dcf798
aburak256 <aburak256@gmail.com>
2021-05-06 02:00:36 +0300

    Replace spinner with skeleton at postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { VStack, Box, Badge, HStack, Text} from '@chakra-ui/layout';
 import { StarIcon } from '@chakra-ui/icons'
 import {API} from "aws-amplify";
-import { Spinner } from "@chakra-ui/react"
+import { Spinner, Image,  Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react"
 import {Link} from 'react-router-dom'
 
 
@@ -26,13 +26,10 @@ export default class PostList extends React.Component {
     return (
         <VStack w="100%">
           {this.state.loading ? 
-            <Spinner
-              thickness="4px"
-              speed="0.65s"
-              emptyColor="gray.200"
-              color="blue.500"
-              size="xl"
-            />
+            <Box w='100%' padding="6" boxShadow="lg" bg="white">
+              <SkeletonCircle size="100" />
+              <SkeletonText mt="4" noOfLines={6} spacing="4" />
+            </Box>
             :  
             <>{this.state.sizeOfArray ? <> {this.state.posts.map(post => 
             <Box w="80%" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="lg" key={post.PK}>         
