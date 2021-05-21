60a8515488444456b18334610180f1b435c25dcd
aburak256 <aburak256@gmail.com>
2021-05-22 02:19:23 +0300

    Add new route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signup from './components/pages/Signup';
 import Topics from './components/pages/Topics';
 import TopicDetail from './components/pages/TopicDetail';
 import PostDetail from './components/pages/PostDetail';
+import Test from './components/pages/Test'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Amplify, {API} from "aws-amplify";
 import awsExports from "./aws-exports";
@@ -23,6 +24,7 @@ function App() {
                     <Route path='/sign-up' exact component={Signup} />
                     <Route path='/topics' exact component={Topics} />
                     <Route exact path="/topics/:topic" component={TopicDetail} />
+                    <Route exact path="/test/:topic" component={Test} />
                     <Route exact path="/posts/:post" component={PostDetail} />
                     {/* <Route path='/about' exact component={About} /> */}
                 </Switch>
