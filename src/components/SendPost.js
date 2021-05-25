fb6c155f0c0a925b41f3eb58778f7c0b74388481
aburak256 <aburak256@gmail.com>
2021-05-26 01:14:46 +0300

    Create: SendPost component

---
+++ src/components/SendPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SendPost extends Component {
+    render() {
+        return (
+            <div>
+                This is a send post component
+            </div>
+        )
+    }
+}
+
+export default SendPost
