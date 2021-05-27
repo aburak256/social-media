28cac83c43745ff61465d75fa3b8b1d09828b3dc
aburak256 <aburak256@gmail.com>
2021-05-27 20:23:01 +0300

    Add: Logo component to easy change

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import { Text } from '@chakra-ui/layout'
+import React, { Component } from 'react'
+
+export class Logo extends Component {
+    render() {
+        return (
+            <div>
+                <Text>
+                    SOCM
+                </Text>
+            </div>
+        )
+    }
+}
+
+export default Logo
