273e84cb15199bd477533b558fb1e1361fbaef93
aburak256 <aburak256@gmail.com>
2021-05-30 17:50:36 +0300

    Add: Messages component. Not finished

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Message extends Component {
+    render() {
+        return (
+            <div>
+                This is the messaging part
+            </div>
+        )
+    }
+}
+
+export default Message
