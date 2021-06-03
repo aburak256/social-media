bab56f09f13465c0664c492f4f8179db123520c4
aburak256 <aburak256@gmail.com>
2021-06-03 15:53:26 +0300

    Add: Function that collects user's posts

--- amplify/backend/function/profileLambda/src/index.py
+++ amplify/backend/function/profileLambda/src/index.py
@@ -13,3 +13,50 @@ def handler(event, context):
       },
       'body': json.dumps('Hello from your new Amplify Python lambda!')
   }
+def collectUserPosts(user):
+    posts = []
+
+    try:
+        postsUserResponse = table.query(
+            KeyConditionExpression=Key('PK').eq('USER#' + user + '#POST'),
+            ScanIndexForward=False
+        )
+    except ClientError as e:
+        print(e.postsUserResponse['Error']['Message'])
+    else:
+        if 'Items' in postsUserResponse and len(postsUserResponse['Items']) >= 1:
+            for userPost in postsUserResponse['Items']:
+                #Collect the original post's information
+                try:
+                    postResponse = table.get_item(
+                        Key={'PK': 'POST#' + userPost['postId'], 'sortKey': 'METADATA'}
+                    )
+                except ClientError as e:
+                    print(e.postResponse['Error']['Message'])
+                else:
+                    if 'Item' in postResponse:
+                        postObject = {}
+                        for info in postResponse['Item']:
+                            if info == 'PK' or info == 'sortKey':
+                                continue
+                            elif info =='dateTime':
+                                dateTimePost = datetime.strptime(postResponse['Item'][info], '%Y-%m-%dT%H:%M:%S.%f')
+                                postObject['dateTime'] = dateTimePost.strftime("%m/%d/%Y, %H:%M:%S")
+                            else:
+                                postObject[info] = postResponse['Item'][info] 
+
+                        #Check if user liked or disliked before
+                        try:
+                            reactionResponse = table.query(
+                                KeyConditionExpression=Key('PK').eq('POST#' + userPost['postId'] + '#REACTION#' + user)
+                            )
+                        except ClientError as e:
+                            print(e.reactionResponse['Error']['Message'])
+                        else:
+                            if 'Items' in reactionResponse and len(reactionResponse['Items']) >= 1:
+                                postObject['Reaction'] = reactionResponse['Items'][0]['text']
+                            posts.append(postObject)
+            
+            return posts
+                                
+
