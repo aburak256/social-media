9f3d04f39a08beb8069809178af68a5b7fd95f2b
aburak256 <aburak256@gmail.com>
2021-06-09 14:28:44 +0300

    Add: Pagination to timeline lambda

--- amplify/backend/function/timelineLambda/src/index.py
+++ amplify/backend/function/timelineLambda/src/index.py
@@ -39,65 +39,88 @@ def handler(event, context):
             user = event['requestContext']['identity']['cognitoAuthenticationProvider'][-36:]
         else:
             return Fail
-        
+
+
         #Collect post links
-        try:
+        if event['queryStringParameters'] != None:
+            #Collect posts starting from last post
+            postRes = table.get_item(
+                Key={'PK': 'POST#' + event['queryStringParameters']['paginator'], 'sortKey': 'METADATA'}
+            )
+            post = postRes['Item']
             postLinks = table.query(
                 KeyConditionExpression=Key('PK').eq('USER#' + user + '#TIMELINE#POST'),
-                ScanIndexForward=False
-            )
-        except ClientError as e:
-            print(e.postLinks['Error']['Message'])
-        else:
-            posts = []
-            if 'Items' in postLinks and len(postLinks['Items']) >= 1:
-                for postLink in postLinks['Items']:
-                    #Collect original post's data
-                    try:
-                        postResponse = table.get_item(
-                            Key={'PK': 'POST#' + postLink['postId'], 'sortKey': 'METADATA'}
-                        )
-                    except ClientError as e:
-                        print(e.postResponse['Error']['Message'])
-                    else:
-                        if 'Item' in postResponse:
-                            postObject = {}
-                            for info in postResponse['Item']:
-                                if info == 'PK' or info == 'sortKey':
-                                    continue
-                                elif info =='dateTime':
-                                    dateTimePost = datetime.strptime(postResponse['Item'][info], '%Y-%m-%dT%H:%M:%S.%f')
-                                    postObject['dateTime'] = dateTimePost.strftime("%m/%d/%Y, %H:%M:%S")
-                                else:
-                                    postObject[info] = postResponse['Item'][info]
-                            #Check if user liked or disliked before
-                            try:
-                                reactionResponse = table.query(
-                                    KeyConditionExpression=Key('PK').eq('POST#' + postLink['postId'] + '#REACTION#' + user)
-                                )
-                            except ClientError as e:
-                                print(e.reactionResponse['Error']['Message'])
-                            else:
-                                if 'Items' in reactionResponse and len(reactionResponse['Items']) >= 1:
-                                    postObject['Reaction'] = reactionResponse['Items'][0]['text']
-                                posts.append(postObject)
-                
-                res={'posts': posts}
-                
-            else:
-                res={'FailMessage': 'There are no posts here'}
-
-            response = {
-                    'statusCode': 200,
-                    'body': json.dumps(res),
-                    'headers': {
-                        'Access-Control-Allow-Headers': '*',
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-                    },
+                ScanIndexForward=False,
+                Limit=30,
+                ExclusiveStartKey={
+                    'PK': 'USER#' + user + '#TIMELINE#POST',
+                    'sortKey': post['dateTime']
                 }
+            )
+            if 'LastEvaluatedKey' in postLinks:
+                cont = 'True'
+            else:
+                cont = 'False'    
+        else:
+            try:
+                postLinks = table.query(
+                    KeyConditionExpression=Key('PK').eq('USER#' + user + '#TIMELINE#POST'),
+                    ScanIndexForward=False,
+                    Limit=30
+                )
+            except ClientError as e:
+                print(e.postLinks['Error']['Message'])
+            else:
+                cont = 'True'
+        posts = []
+        if 'Items' in postLinks and len(postLinks['Items']) >= 1:
+            for postLink in postLinks['Items']:
+                #Collect original post's data
+                try:
+                    postResponse = table.get_item(
+                        Key={'PK': 'POST#' + postLink['postId'], 'sortKey': 'METADATA'}
+                    )
+                except ClientError as e:
+                    print(e.postResponse['Error']['Message'])
+                else:
+                    if 'Item' in postResponse:
+                        postObject = {}
+                        for info in postResponse['Item']:
+                            if info == 'PK' or info == 'sortKey':
+                                continue
+                            elif info =='dateTime':
+                                dateTimePost = datetime.strptime(postResponse['Item'][info], '%Y-%m-%dT%H:%M:%S.%f')
+                                postObject['dateTime'] = dateTimePost.strftime("%m/%d/%Y, %H:%M:%S")
+                            else:
+                                postObject[info] = postResponse['Item'][info]
+                        #Check if user liked or disliked before
+                        try:
+                            reactionResponse = table.query(
+                                KeyConditionExpression=Key('PK').eq('POST#' + postLink['postId'] + '#REACTION#' + user)
+                            )
+                        except ClientError as e:
+                            print(e.reactionResponse['Error']['Message'])
+                        else:
+                            if 'Items' in reactionResponse and len(reactionResponse['Items']) >= 1:
+                                postObject['Reaction'] = reactionResponse['Items'][0]['text']
+                            posts.append(postObject)
+            
+            res={'posts': posts, 'cont': cont}
+            
+        else:
+            res={'FailMessage': 'There are no posts here'}
 
-            return response
+        response = {
+                'statusCode': 200,
+                'body': json.dumps(res),
+                'headers': {
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                },
+            }
+
+        return response
 
   
     return {
