10f046b42546d03af724acfe4c47ac8ceb9c0ef1
aburak256 <aburak256@gmail.com>
2021-06-15 17:15:11 +0300

    Add: Fixed sidebar

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import awsExports from "./aws-exports";
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import Sidebar from './components/Sidebar';
 import {useState, useEffect} from 'react'
+import {HStack, VStack, Box} from '@chakra-ui/react'
 Amplify.configure(awsExports);
 
 
@@ -34,22 +35,28 @@ function App() {
         <div className = "App">
             <Router>
                 <Navbar handleSearch={handleSearch}/>
-                {search ? <SearchResults text={searchText} onClose={() => setSearch(false)}/> : 
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/sign-up' exact component={Signup} />
-                    <Route path='/topics' exact component={Topics} />
-                    <Route path='/Bookmarks' exact component={Bookmarks} />
-                    <Route path='/Messages' exact component={Messages} />
-                    <Route exact path="/topics/:topic" component={TopicDetail} />
-                    <Route exact path="/test/:topic" component={Test} />
-                    <Route exact path="/profile/:profile" component={Profile} />
-                    <Route exact path="/profile/" component={Profile} />
-                    <Route exact path="/posts/:post" component={PostDetail} />
-                    {/* <Route path='/about' exact component={About} /> */}
-                </Switch>
-                }
-                
+                <HStack align='top' spacing='1' bg='gray.100' >
+                    <Box w='23%' style={{position: 'fixed', top: '5'}}>
+                        <Sidebar />
+                    </Box>
+                    {search ? <SearchResults text={searchText} onClose={() => setSearch(false)}/> : 
+                    <VStack w='50%' pl='43%' minH='100vh'>
+                        <Switch>
+                            <Route path='/' exact component={Home} />
+                            <Route path='/sign-up' exact component={Signup} />
+                            <Route path='/topics' exact component={Topics} />
+                            <Route path='/Bookmarks' exact component={Bookmarks} />
+                            <Route path='/Messages' exact component={Messages} />
+                            <Route exact path="/topics/:topic" component={TopicDetail} />
+                            <Route exact path="/test/:topic" component={Test} />
+                            <Route exact path="/profile/:profile" component={Profile} />
+                            <Route exact path="/profile/" component={Profile} />
+                            <Route exact path="/posts/:post" component={PostDetail} />
+                            {/* <Route path='/about' exact component={About} /> */}
+                        </Switch>
+                    </VStack>
+                    }
+                </HStack>
             </Router>
         </div >
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,65 +14,33 @@ import {
     DrawerCloseButton,
     useDisclosure,
     Input,
+    VStack,
   } from "@chakra-ui/react"
 
   function  Sidebar () {
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = React.useRef()
         return (
-            <Box pl='2' pt='2'>
-                <Button ref={btnRef}
-                    colorScheme="black"
-                    _hover={{
-                        bgGradient: "linear(to-r, gray.500, gray.800)",
-                    }} 
-                    onClick={onOpen}
-                >
-                    <ArrowRightIcon/>
-                </Button>
-                <Drawer
-                    isOpen={isOpen}
-                    placement="left"
-                    onClose={onClose}
-                    finalFocusRef={btnRef}
-                >
-                    <DrawerOverlay  />
-                    <DrawerContent bgGradient="linear(to-b, teal.200,green.200)">
-                    <DrawerCloseButton />
-                    <DrawerHeader><Logo /></DrawerHeader>
-
-                    <DrawerBody>
-                        <Link  to='/topics'>
-                            <Button mt='4' bg='teal.500' w='100%'>
-                                Topics
-                            </Button>
-                        </Link>
-                        <Link to='/Profile'>
-                            <Button mt='4' bg='teal.500' w='100%'>
-                                Profile
-                            </Button>
-                        </Link>
-                        <Link to='/Messages'>
-                            <Button mt='4' bg='blue.500' w='100%'>
-                                Messages
-                            </Button>
-                        </Link>
-                        <Link to='/Bookmarks'>
-                            <Button mt='4' bg='blue.500' w='100%'>
-                                Bookmarks
-                            </Button>
-                        </Link>
-                    </DrawerBody>
-
-                    <DrawerFooter>
-                        <Button bg='teal.500' variant="outline" mr={3} onClick={onClose}>
-                        Close
-                        </Button>
-                    </DrawerFooter>
-                    </DrawerContent>
-                </Drawer>
-            </Box>
+            <VStack pl='2' pt='2' h='100vh' maxW='100%' align='left'>
+                <Link  to='/topics'>
+                    <Button mt='4'  w='100%'>
+                        Topics
+                    </Button>
+                </Link>
+                <Link to='/Profile'>
+                    <Button mt='4'  w='100%'>
+                        Profile
+                    </Button>
+                </Link>
+                <Link to='/Messages'>
+                    <Button mt='4' w='100%'>
+                        Messages
+                    </Button>
+                </Link>
+                <Link to='/Bookmarks'>
+                    <Button mt='4'  w='100%'>
+                        Bookmarks
+                    </Button>
+                </Link>                
+            </VStack>
         )
 }
 
